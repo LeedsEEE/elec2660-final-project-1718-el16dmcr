fb83e218c3ef9f9b4f31948e5c20114841632d47
David Russell [el16dmcr] <el16dmcr@eentpc249.leeds.ac.uk>
2017-12-06 13:50:42 +0000

    Remade my instruction screens so they are easier to read, started doing some more colour setup for my games.

--- Axon Evolution/OpticalConundrumDraw.m
+++ Axon Evolution/OpticalConundrumDraw.m
@@ -10,12 +10,30 @@
 
 @implementation OpticalConundrumDraw
 
-/*
+UIColor *RGB6(float r, float g, float b)
+{
+    return [UIColor colorWithRed:r/255.0f
+                           green:g/255.0f
+                            blue:b/255.0f
+                           alpha:1.0];
+}
+
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
-    // Drawing code
+    
+    CGColorRef opticalConundrumPurple = [RGB6(161, 0, 255) CGColor];
+    
+    CGFloat width = [UIScreen mainScreen].bounds.size.width;
+    CGFloat height = [UIScreen mainScreen].bounds.size.height;
+    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextClearRect(context, self.bounds);
+    
+    CGContextSetFillColorWithColor(context, opticalConundrumPurple);
+    CGContextFillRect(context, CGRectMake(0, 0, width, height));
+    
 }
-*/
+
 
 @end
